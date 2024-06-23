d493fdf0c086ac758f904449d9269728696d7227
pecheylauren02 <pechey.lauren02@gmail.com>
2024-06-23 09:46:47 +0000

    make index.js file

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold my-8">Welcome to Tickt Event App</h1>
+      <p className="text-xl">This is the home page content.</p>
+      {/* Add more content or components as needed */}
+    </div>
+  );
+};
+
+export default Home;
